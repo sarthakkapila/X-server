a8fc084a252715dfb681403c1d6e6b507511f5f9
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-02-04 01:58:18 +0530

    commit

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { startApolloServer } from "./app";
-
-startApolloServer().then(app => {
-  app.listen({ port: 8000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:8000`)
-  );
-});

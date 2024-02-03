70c3a40ed00e1cabd457186deb4ee21ae7ebfc15
Sarthak <112886451+sarthakkapila@users.noreply.github.com>
2024-02-04 01:53:39 +0530

    Delete build directory

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-(0, app_1.startApolloServer)().then(app => {
-    app.listen({ port: 8000 }, () => console.log(`🚀 Server ready at http://localhost:8000`));
-});

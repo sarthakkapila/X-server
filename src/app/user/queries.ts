317eda2a4323433d590045a98ca14b7d183e4cea
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-02-10 02:33:21 +0530

    added login detection

--- src/app/user/queries.ts
+++ src/app/user/queries.ts
@@ -1,6 +1,18 @@
+import { graphql } from "graphql";
+
 export const queries = `#graphql
     verifyGoogleToken(token: String!): String
     getCurrentUser: User
     getUserById(id: ID!): User
-
 `;
+
+export const getCurrentUserQuery =`
+    query GetCurrentuser{
+        getCurrentUser{
+            id
+            email
+            Firstname
+            profileimageURL
+    }
+    }
+    `;
